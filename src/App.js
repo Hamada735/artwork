b5c39a40522492b5b10947d363f3e313a051dc42
hamada.alosh <mohamad.75.alosh@gmail.com>
2022-11-11 02:06:51 +0000

    added tailwind, also added relevant compontents with props passing

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import Paintings from './components/paintings';
+import Navbar from './components/navbar';
 
 function App() {
   return (
     <div className="App">
+      <Navbar/>
+      <Paintings/>
       <p>"g check"</p>
     </div>
   );
